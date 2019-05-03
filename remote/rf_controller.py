b41bb693e45db10d23e08e1ccbb572f2410f160e
Abyll Breil <abyllbreil@gmail.com>
2019-05-03 00:08:45 -0600

    Fixed channel iteration. Added time delta tracking.

--- remote/rf_controller.py
+++ remote/rf_controller.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
 
     RX=3
     TX=4
+    MASK = 0b111111111111
+    ADDRESS = 0b101010101010
     CHANNELS = {
         "up": 4,
         "down": 2,
@@ -15,15 +17,24 @@ if __name__ == "__main__":
         "right": 0,
         "fire": 6
         }
-
+    last_call = time.time()
     # define optional callback for received codes.
 
     def rx_callback(code, bits, gap, t0, t1):
-        ch = {}
-        for name, channel in CHANNELS:
-            ch[name] = code & 1 << channel != 0
-        print("code={} bits={}\nchannels={})".
-            format(code, output, ch))
+        global last_call
+        output = "{0:b}".format(code)
+        address = code >> 11
+        if address == ADDRESS:
+            code = code & MASK
+            ch = {}
+            for name, channel in CHANNELS.iteritems():
+                ch[name] = code & 1 << channel != 0
+            cur_time = time.time()
+            delay = (cur_time-last_call) # ms
+            last_call = cur_time
+            print("code={} bits={}\nchannels={}\ndelay={:.5})".
+                format(code, output, ch, delay))
+            
 
     pi = pigpio.pi() # Connect to local Pi.
 
